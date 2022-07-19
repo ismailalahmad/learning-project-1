31a31206d970a1aabf3157147c102c06bac91721
ismail al ahmad <asmayllalahmad90@gmail.com>
2022-07-19 16:21:43 +0300

    added new file neames secondclass

---
+++ src/com/company/secondclass.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class secondclass {
+
+    static void d(){
+        System.out.println("1 edit");
+        System.out.println("2 edit");
+        System.out.println("3 edit");
+    }
+}
+go
